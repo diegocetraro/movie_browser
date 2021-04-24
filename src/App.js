8f14d1821a2478d111613f7b76e6bd09f2f4fd78
Juan Diego Aranda <diego.aranda0708@gmail.com>
2021-04-24 12:51:11 -0500

    Browser Router settings added, React Links implemented

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import AboutView from './Components/AboutView'
+import { Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
         <Navbar />
-        <Home />
-        <AboutView />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about" component={AboutView}>
+            <AboutView />
+          </Route>
+        </Switch>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,34 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 class Navbar extends React.Component{
     render(){
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient p-3">
             <div className="container-fluid">
-                <a className="navbar-brand" href="gotosomewhereelse">Movie Browser</a>
+                <Link className="navbar-brand" to="/">Movie Browser</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="gotosomewhereelse">Home</a>
+                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="gotosomewhereelse">Link</a>
+                        <Link className="nav-link" to="/about">About Us</Link>
                     </li>
                     <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="gotosomewhereelse " id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                        <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</Link>
                         <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="gotosomewhereelse">Action</a></li>
-                            <li><a className="dropdown-item" href="gotosomewhereelse">Another action</a></li>
+                            <li><Link className="dropdown-item" to="/">Action</Link></li>
+                            <li><Link className="dropdown-item" to="/">Another action</Link></li>
                             <li><hr className="dropdown-divider" /></li>
-                            <li><a className="dropdown-item" href="gotosomewhereelse">Something else here</a></li>
+                            <li><Link className="dropdown-item" to="/">Something else here</Link></li>
                         </ul>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link disabled" href="gotosomewhereelse" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        <Link className="nav-link disabled" to="/" tabIndex="-1" aria-disabled="true">Coming Soon</Link>
                     </li>
                 </ul>
                 <form className="d-flex">
